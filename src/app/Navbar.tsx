6e20222ec3245868e359649731319212ae5f02a6
Buchi Michelle Okonicha <buchi@Buchis-MacBook-Air.local>
2024-03-26 16:56:32 +0100

    y

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,12 +1,28 @@
+'use client'
+
 import Image from "next/image"
+import { useState } from "react";
+
+
 export default function Navbar() {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const hideNavBar = () => {
+        setIsOpen(false);
+    };
+    const showNavBar = () => {
+        setIsOpen(true);
+    }
+
+
     return (
         <div className="bg-white">
             <header className="absolute inset-x-0 top-0 z-50">
                 <nav className="flex items-center justify-between py-6 lg:px-8" aria-label="Global">
 
                     <div className="flex lg:hidden">
-                        <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+                        <button type="button" onClick={showNavBar} className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
                             <span className="sr-only">Open main menu</span>
                             <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
@@ -35,20 +51,20 @@ export default function Navbar() {
                         <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Support </a>
                         <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Contact Us</a>
                         <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Try for Free </a>
-                        <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Sin In </a>
+                        <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Sign In </a>
                     </div>
                 </nav>
                 {/* <!-- Mobile menu, show/hide based on menu open state. --> */}
-                <div className="lg:hidden" role="dialog" aria-modal="true">
+                <div className={`lg:hidden ${!isOpen ? 'hidden' : ''}`} role="dialog" aria-modal="true">
                     {/* <!-- Background backdrop, show/hide based on slide-over state. --> */}
                     <div className="fixed inset-0 z-50"></div>
                     <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                         <div className="flex items-center justify-between">
                             <a href="#" className="-m-1.5 p-1.5">
-                                <span className="sr-only">Your Company</span>
+                                <span className="sr-only">Fugoku</span>
                                 {/* <Image width={50} height={50} className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" /> */}
                             </a>
-                            <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700">
+                            <button type="button" onClick={hideNavBar} className="-m-2.5 rounded-md p-2.5 text-gray-700">
                                 <span className="sr-only">Close menu</span>
                                 <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,36 +5,36 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <div className="flex items-center my-10 justify-between" aria-label="Global">
+      <div className=" mt-20 sm:mt-0 flex-col sm:flex-row mx-2 flex items-center my-10 justify-between" aria-label="Global">
 
-        <div className="hidden lg:flex lg:gap-x-5 mx-auto max-w-2xl">
+        <div className="lg:flex lg:gap-x-5 mx-auto max-w-2xl">
 
           <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Fugoku cloud hosting will empower platform</h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+            <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-6xl ">Fugoku cloud hosting will empower platform</h1>
+            <p className="mt-6 sm:text-lg  text-sm leading-8 text-gray-600">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a href="#" className="rounded-md bg-[#00d2ff] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#00d2ff]">Get started</a>
               <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
             </div>
           </div>
         </div>
-        <div className="hidden lg:flex lg:gap-x-5 mx-auto max-w-2xl  ">
+        <div className=" lg:flex lg:gap-x-5 mx-auto max-w-2xl  ">
 
           <Image width={600} height={600} src="/assets/fugoku-cloud1.jpeg" alt='side-image' />
         </div>
       </div>
-      <div className="bg-[#000435] py-6 flex items-center justify-between" aria-label="Global">
+      <div className="bg-[#000435] py-6 flex-col sm:flex-row gap-y-10 sm:gap-y-0 flex items-center justify-between" aria-label="Global">
 
-        <div className="hidden lg:flex lg:gap-x-5 mx-auto max-w-2xl ">
+        <div className=" lg:flex lg:gap-x-5 mx-auto max-w-2xl ">
 
           <div className="text-center">
-            <h2 className="text-2xl font-bold tracking-tight text-white sm:text-3xl">AI Powered Application</h2>
+            <h2 className="text-1xl font-bold tracking-tight text-white sm:text-3xl">AI Powered Application</h2>
 
-            <p className="text-lg leading-8 text-white">sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+            <p className="sm:text-lg text-sm leading-8 text-white">sunt amet fugiat veniam occaecat fugiat aliqua.</p>
 
           </div>
         </div>
-        <div className="hidden lg:flex lg:gap-x-5 mx-auto max-w-2xl">
+        <div className=" lg:flex lg:gap-x-5 mx-auto max-w-2xl">
 
           {/* form starts */}
           <form className="w-full max-w-sm">
@@ -51,9 +51,9 @@ export default function Home() {
       </div>
       {/* threes starts */}
       <h2 className="text-2xl font-bold tracking-tight text-center items-center py-10 pb-20 text-black sm:text-3xl">Discover what’s happening on Fugoku</h2>
-      <div className="text-black  lg:gap-x-10 flex content-center items-center  justify-items-center m-auto justify-center" aria-label="Global">
+      <div className="text-black gap-y-10 m-5 sm:gap-y-0 lg:gap-x-10 flex-wrap flex content-center items-center  justify-items-center sm:m-auto justify-center" aria-label="Global">
         {/* <div className="flex-1 w-32 ..."> */}
-        <div className="flex-1 w-32 max-w-sm border border-black-500 rounded rounded-3xl overflow-hidden shadow-lg">
+        <div className="sm:flex-1 sm:w-32 max-w-sm border border-black-500 rounded rounded-3xl overflow-hidden shadow-lg">
           <div className="flex content-center justify-center items-center">
             <Image width={200} height={200} className="" src="/assets/cloud4.png" alt="Sunset in the mountains" />
           </div>
@@ -71,7 +71,7 @@ export default function Home() {
         </div>
         {/* </div> */}
         {/* <div className="flex-1 w-32 ..."> */}
-        <div className="flex-1 w-32 max-w-sm border border-black-500 rounded rounded-3xl overflow-hidden shadow-lg">
+        <div className="sm:flex-1 sm:w-32 max-w-sm border border-black-500 rounded rounded-3xl overflow-hidden shadow-lg">
           <div className="flex content-center justify-center items-center">
             <Image width={200} height={200} className="" src="/assets/cloud3.png" alt="Sunset in the mountains" />
           </div>
@@ -89,7 +89,7 @@ export default function Home() {
         </div>
         {/* </div> */}
         {/* <div className="flex-1 w-32 ..."> */}
-        <div className="flex-1 w-32 max-w-sm border border-black-500 rounded rounded-3xl overflow-hidden shadow-lg">
+        <div className="sm:flex-1 sm:w-32 max-w-sm border border-black-500 rounded rounded-3xl overflow-hidden shadow-lg">
           <div className="flex content-center justify-center items-center">
             <Image width={200} height={200} className="" src="/assets/cloud4.png" alt="Sunset in the mountains" />
           </div>
@@ -109,21 +109,21 @@ export default function Home() {
       </div>
       {/* threes ends */}
       {/* make starts */}
-      <div className="flex items-center lg:gap-x-10 justify-between py-48 px-10" aria-label="Global">
-        <div className="hidden lg:flex lg:gap-x-5 mx-auto max-w-2xl">
+      <div className="flex-col sm:flex-row gap-y-10 sm:gap-y-0 flex items-center lg:gap-x-10 justify-between py-48 px-10" aria-label="Global">
+        <div className=" lg:flex lg:gap-x-5 mx-auto max-w-2xl">
           <div className="text-center w-55">
             <h4 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Finding solutions</h4>
             <p className="mt-6 text-lg leading-8 text-gray-600">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
           </div>
         </div>
-        <div className="hidden shadow-lg border border-black-500 rounded rounded-3xl overflow-hidden  shadow-black lg:flex lg:gap-x-5 mx-auto max-w-2xl">
+        <div className=" shadow-lg border border-black-500 rounded rounded-3xl overflow-hidden  shadow-black lg:flex lg:gap-x-5 mx-auto max-w-2xl">
           <Image width={600} height={600} src="/assets/cloud1.jpeg" alt='side-image' />
         </div>
       </div>
 
-      <div className="text-black lg:gap-x-10 flex content-center items-center  justify-items-center mx-10 justify-center" aria-label="Global">
+      <div className="text-black m-auto lg:gap-x-10 gap-y-10 sm:m-5 sm:gap-y-0 flex-col sm:flex-row flex content-center items-center  justify-items-centersm:mx-10 justify-center" aria-label="Global">
         {/* <div className="flex-1 w-32 ..."> */}
-        <div className="h-[50rem] content-center items-center justify-center flex-1 w-20 max-w-sm border border-black-500 rounded rounded-3xl overflow-hidden shadow-lg">
+        <div className="sm:h-[50rem] w-110 content-center items-center justify-center sm:flex-1 sm:w-20 sm:max-w-sm border border-black-500 rounded rounded-3xl overflow-hidden shadow-lg">
 
           <Image width={500} height={500} className="w-full content-cent" src="/assets/cloud4.png" alt="Sunset in the mountains" />
 
@@ -212,33 +212,33 @@ export default function Home() {
 
       </div>
       {/* </div> */}
-      <div className="flex items-center  lg:gap-x-5 shadow-lg shadow-black rounded rounded-3xl justify-between my-48 mx-24 px-10" aria-label="Global">
-        <div className="hidden lg:flex lg:gap-x-5 mx-auto max-w-2xl">
+      <div className="flex-col sm:flex-row gap-y-10 sm:gap-y-0 flex items-center lg:gap-x-5 shadow-lg shadow-black rounded rounded-3xl justify-between my-48 mx-5 sm:mx-24 sm:px-10" aria-label="Global">
+        <div className=" lg:flex lg:gap-x-5 mx-auto sm:max-w-2xl">
           <div className="text-center">
             <h4 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">mimi solutions</h4>
             <p className="mt-6 text-lg leading-8 text-gray-600">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
           </div>
         </div>
-        <div className="hidden  rounded-3xl rounded lg:flex mx-auto max-w-2xl">
-          <Image width={600} height={600} src="/assets/cloud1.jpeg" alt='side-image' />
+        <div className="rounded-3xl rounded flex mx-auto max-w-2xl">
+          <Image width={600} className="" height={600} src="/assets/cloud1.jpeg" alt='side-image' />
         </div>
       </div>
-      <h4 className="text-2xl py-10 text-center content-center font-bold tracking-tight text-gray-900 sm:text-3xl">Optimize your infrastructure with Fugoku</h4>
+      <h4 className="text-2xl mx-2 py-10 text-center content-center font-bold tracking-tight text-gray-900 sm:text-3xl">Optimize your infrastructure with Fugoku</h4>
       <div className="hidden mb-10 shadow-lg  shadow-black rounded-3xl lg:flex lg:gap-x-5 center mx-auto max-w-2xl">
         <Image width={500} height={500} className="rounded-3xl" src="/assets/cloud2.png" alt='side-image' />
       </div>
-      <div className="bg-[#000435] py-6 flex items-center justify-between" aria-label="Global">
+      <div className="bg-[#000435] py-6 flex-col sm:flex-row gap-y-10 sm:gap-y-0 flex items-center justify-between" aria-label="Global">
 
-        <div className="hidden lg:flex lg:gap-x-5 mx-auto max-w-2xl ">
+        <div className=" lg:flex lg:gap-x-5 mx-auto max-w-2xl ">
 
           <div className="text-center">
-            <h2 className="text-2xl font-bold tracking-tight text-white sm:text-3xl">Fugoku AI Powered Application</h2>
+            <h2 className="text-1xl font-bold tracking-tight text-white sm:text-3xl">Fugoku AI Powered Application</h2>
 
-            <p className="text-lg leading-8 text-white">sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+            <p className="sm:text-lg text-sm leading-8 text-white">sunt amet fugiat veniam occaecat fugiat aliqua.</p>
 
           </div>
         </div>
-        <div className="hidden lg:flex lg:gap-x-5 mx-auto max-w-2xl">
+        <div className=" lg:flex lg:gap-x-5 mx-auto max-w-2xl">
 
           {/* form starts */}
           <form className="w-full max-w-sm">
@@ -255,9 +255,9 @@ export default function Home() {
       </div>
       {/* get more value */}
       <h2 className="text-2xl font-bold tracking-tight text-center items-center py-10 pb-20 text-black sm:text-3xl">Get more value</h2>
-      <div className="text-black bg-[#daf0ff] py-10 mx-10 rounded rounded-3xl lg:gap-x-10 flex content-center items-center  justify-items-center m-auto justify-center" aria-label="Global">
+      <div className="text-black bg-[#daf0ff] py-10 mx-10 rounded rounded-3xl gap-y-10 sm:gap-y-0 lg:gap-x-10 flex-col sm:flex-row flex content-center items-center  justify-items-center m-auto justify-center" aria-label="Global">
         {/* <div className="flex-1 w-32 ..."> */}
-        <div className="flex-1 text-black w-32 max-w-sm overflow-hidden shadow-lg">
+        <div className="flex-1  mb-5 sm:mb-0 text-black sm:w-32 sm:max-w-sm overflow-hidden shadow-lg">
 
           <div className="px-6 py-4 
           ">
@@ -270,7 +270,7 @@ export default function Home() {
         </div>
         {/* </div> */}
         {/* <div className="flex-1 w-32 ..."> */}
-        <div className="flex-1 w-32 max-w-sm  overflow-hidden shadow-lg">
+        <div className="flex-1 sm:w-32 sm:max-w-sm  mb-5 sm:mb-0 overflow-hidden shadow-lg">
 
           <div className="px-6 py-4 ">
             <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
@@ -282,8 +282,7 @@ export default function Home() {
         </div>
         {/* </div> */}
         {/* <div className="flex-1 w-32 ..."> */}
-        <div className="flex-1 w-32 max-w-sm overflow-hidden shadow-lg">
-
+        <div className="flex-1 sm:w-32 mb-5 sm:mb-0 sm:max-w-sm overflow-hidden shadow-lg">
           <div className="px-6 py-4 ">
             <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
             <p className=" text-base">
@@ -296,23 +295,22 @@ export default function Home() {
       </div>
       {/* get more value ends */}
       <h4 className="text-2xl font-bold my-10 tracking-tight text-center items-center py-10 pb-20 text-black sm:text-3xl">See how customers are innovating</h4>
-
-      <div className="flex items-center  lg:gap-x-5 shadow-lg shadow-black rounded rounded-3xl justify-between mb-48 mx-24 px-10" aria-label="Global">
-        <div className="hidden lg:flex lg:gap-x-5 mx-auto max-w-2xl">
+      <div className="flex-col sm:flex-row gap-y-10 sm:gap-y-0 flex items-center lg:gap-x-5 shadow-lg shadow-black rounded rounded-3xl justify-between my-48 mx-5 sm:mx-24 sm:px-10" aria-label="Global">
+        <div className=" lg:flex lg:gap-x-5 mx-auto sm:max-w-2xl">
           <div className="text-center">
             <h4 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">mimi solutions</h4>
             <p className="mt-6 text-lg leading-8 text-gray-600">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
           </div>
         </div>
-        <div className="hidden  rounded-3xl rounded lg:flex mx-auto max-w-2xl">
-          <Image width={600} height={600} src="/assets/cloud1.jpeg" alt='side-image' />
+        <div className="rounded-3xl rounded flex mx-auto max-w-2xl">
+          <Image width={600} className="" height={600} src="/assets/cloud1.jpeg" alt='side-image' />
         </div>
       </div>
       {/* threes2 starts */}
       <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">Take the next step</h2>
-      <div className="text-black mb-10  lg:gap-x-10 flex content-center items-center  justify-items-center m-auto justify-center" aria-label="Global">
+      <div className="text-black mb-10 flex-col sm:flex-row sm:gap-y-0 gap-y-10 lg:gap-x-10 flex content-center items-center  justify-items-center m-auto justify-center" aria-label="Global">
         {/* <div className="flex-1 w-32 ..."> */}
-        <div className="flex-1 w-32 max-w-sm border border-black-500 rounded rounded-3xl overflow-hidden shadow-lg">
+        <div className="flex-1 sm:w-32 sm:max-w-sm border border-black-500 rounded rounded-3xl overflow-hidden shadow-lg">
           <div className="flex content-center justify-center items-center">
             <Image width={200} height={200} className="" src="/assets/cloud2.png" alt="Sunset in the mountains" />
           </div>
@@ -330,7 +328,7 @@ export default function Home() {
         </div>
         {/* </div> */}
         {/* <div className="flex-1 w-32 ..."> */}
-        <div className="flex-1 w-32 max-w-sm border border-black-500 rounded rounded-3xl overflow-hidden shadow-lg">
+        <div className="flex-1 sm:w-32 sm:max-w-sm border border-black-500 rounded rounded-3xl overflow-hidden shadow-lg">
           <div className="flex content-center justify-center items-center">
             <Image width={200} height={200} className="" src="/assets/cloud1.jpeg" alt="Sunset in the mountains" />
           </div>
@@ -348,7 +346,7 @@ export default function Home() {
         </div>
         {/* </div> */}
         {/* <div className="flex-1 w-32 ..."> */}
-        <div className="flex-1 w-32 max-w-sm border border-black-500 rounded rounded-3xl overflow-hidden shadow-lg">
+        <div className="flex-1 sm:w-32 sm:max-w-sm border border-black-500 rounded rounded-3xl overflow-hidden shadow-lg">
           <div className="flex content-center justify-center items-center">
             <Image width={200} height={200} className="" src="/assets/cloud2.png" alt="Sunset in the mountains" />
           </div>
