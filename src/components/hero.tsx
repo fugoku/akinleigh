5213813bb1e0b941f77ebe4a996ab66b840cb8a4
riri <richardokonicha@gmail.com>
2024-05-05 12:49:36 +0100

    updated ui

---
+++ src/components/hero.tsx
@@ -0,0 +1,67 @@
+import WorldMap from "@/components/world-section";
+import Dust from "@/components/dust"
+import Bubble from "@/components/bubble"
+import { Suspense } from "react";
+
+
+export default function Hero() {
+  return (
+    <div className="bg-fill">
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          aria-hidden="true"
+        >
+            
+        </div>
+
+        <div className="mx-auto max-w-2xl py-24 sm:py-48 lg:py-24">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+              <span className="text-primary">Cloud Computing</span> Services and
+              Cloud <span className="text-primary">Consultancy</span>
+            </h1>
+            <p className="mt-6 text-lg leading-8 text-white font-Montserrat">
+              We offer a comprehensive suite of cloud services, including
+              infrastructure as a Service (IaaS), Platform as a Service (PaaS),
+              and Software as a Service (SaaS) to meet your specific needs. Our
+              expert cloud consultants will guide you through every step of your
+              cloud journey, from strategy and migration to ongoing optimization
+              and support.
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <a
+                href="#"
+                className="rounded-sm bg-primary px-5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Get started
+              </a>
+              <a
+                href="#"
+                className="text-sm font-semibold leading-6 text-white"
+              >
+                Schedule Appointment <span aria-hidden="true">→</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div
+          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+          aria-hidden="true"
+        ></div>
+      </div>
+
+      <section className="pb-14 ">
+          <div className="mx-auto max-w-5xl bg-fade ">
+            
+            {/* <WorldMap/> */}
+            {/* <Suspense fallback="null">
+                <Bubble></Bubble>
+            </Suspense> */}
+            {/* <Dust/> */}
+            
+            </div>
+        </section>
+    </div>
+  );
+}
--- src/components/world-section.tsx
+++ src/components/world-section.tsx
@@ -11,7 +11,7 @@ const Lottie = dynamic(() => import('lottie-react'), {
 export default function WorldMap() {
   return (
     <>
-      <div className="relative isolate px-6 pt-14 lg:px-8 z-10">
+      <div className="relative isolate px-6 pt-14 lg:px-8 z-10 py-8">
         <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" />
 
         <div className="animate-fade-in-once lg:h-[67vh] absolute -z-50  left-0 top-0">
@@ -21,10 +21,9 @@ export default function WorldMap() {
           />
           ;
         </div>
-        <div className="mx-auto max-w-2xl py-24 sm:py-48 lg:py-56">
+        <div className="mx-auto max-w-2xl ">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full py-1 px-3 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              fugoku
               <a href="#" className="font-semibold text-indigo-600">
                 <span className="absolute inset-0" aria-hidden="true" />
                 Read more
@@ -34,32 +33,18 @@ export default function WorldMap() {
           </div>
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              BuildEasy. DeployFast.
+              Build Easily, Deploy Fast.
             </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
+            
+            {/* <p className="mt-6 text-lg leading-8 text-gray-600">
               Streamline devops and deployment process of your business
               applications, services, and databases with artificial
               intelligence.
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
-                className="rounded-xs bg-black px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Get started
-              </a>
-              <a
-                href="#"
-                className="text-sm font-semibold leading-6 text-gray-900"
-              >
-                Marketplace<span aria-hidden="true">→</span>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="animate-fade-in-once lg:h-[67vh]">
-          <Lottie animationData={greenifydata} />;
-        </div>
+            </p> */}
+            
+          </div> 
+        </div> 
+     
       </div>
     </>
   );
