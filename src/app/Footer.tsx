270a8bb536a37e01d7a52f88781c4876340dc0e6
Buchi Michelle Okonicha <buchi@Buchis-MacBook-Air.local>
2024-05-07 16:07:19 +0100

    fix footer layout

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -15,7 +15,6 @@ export default function Footer() {
           stroke-width="50"
         />
       </svg>
-
       <div className="mx-auto w-full max-w-screen-xl">
         <div className="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
           <div>
